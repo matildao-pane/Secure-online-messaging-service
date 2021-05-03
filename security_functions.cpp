ad655ad2542f43c203c6ce54b4ce28c74d4da4d3
Giulio <giulio.silve92@gmail.com>
2021-05-03 19:03:22 +0200

    test run symenc

--- security_functions.cpp
+++ security_functions.cpp
@@ -2,16 +2,21 @@
 #include <openssl/evp.h>
 #include <openssl/pem.h>
 #include <openssl/rand.h>
-
-#include <limits.h> 
-
+#include <openssl/err.h>
+#include <iostream> 
+#include <stdlib.h>
+#include <stdio.h>
+#include <limits.h>
+#include <string.h> 
+using namespace std;
 //Asymmetric encription and decription. for initial exange and negotiation(?)
+/*
 envelope_seal(){}
 envelope_open(){}
 // Diffie-Hellman for session key
 dh_generate_params(){} //p,g,key couple
 dh_derive_shared_secret(){}
-
+*/
 //simmetric encription and decription using generated key
 int cbc_encrypt(unsigned char *plaintext, int plaintext_len, unsigned char *key, unsigned char *ciphertext){
 	int ret;
@@ -35,22 +40,20 @@ int cbc_encrypt(unsigned char *plaintext, int plaintext_len, unsigned char *key,
 	//for blocco
 	ret=EVP_EncryptUpdate(ctx, ciphertext+ciphertext_len, &update_len, plaintext, plaintext_len);
 	if(ret!=1){cerr<<"EncryptUpdate Error";exit(1);}
-	total_len += update_len;
+	ciphertext_len += update_len;
 	
 	ret=EVP_EncryptFinal(ctx, ciphertext+ciphertext_len, &update_len);
 	if(ret!=1){cerr<<"EncryptFinal Error";exit(1);}
-	total_len += update_len;
+	ciphertext_len += update_len;
 
 	EVP_CIPHER_CTX_free(ctx);
 #pragma optimize("",off)
 	memset(plaintext,0, plaintext_len);
 #pragma optimize("",on)
-	free(plaintext);
-
 	return ciphertext_len;
 }
 
-cbc_decrypt(unsigned char *ciphertext, int ciphertext_len, unsigned char *key, unsigned char *plaintext){
+int cbc_decrypt(unsigned char *ciphertext, int ciphertext_len, unsigned char *key, unsigned char *plaintext){
 	int ret;
 	int plaintext_len;
 	const EVP_CIPHER* cipher = EVP_aes_128_cbc();
@@ -59,15 +62,16 @@ cbc_decrypt(unsigned char *ciphertext, int ciphertext_len, unsigned char *key, u
 
 	unsigned char *iv = (unsigned char *)malloc(iv_len);
 	memcpy(iv, ciphertext, iv_len);
-	
+	ciphertext+=iv_len;
+	ciphertext_len-=iv_len;
 	EVP_CIPHER_CTX *ctx = EVP_CIPHER_CTX_new();
 	if(!ctx) {cerr<<"EVP_CIPHER_CTX_new Error";exit(1);}
 	ret=EVP_DecryptInit(ctx, cipher, key, iv);
 	if(ret!=1){cerr<<"DecryptInit Error";exit(1);}
-	plaintext_len=iv_len;
+	plaintext_len=0;
 	int update_len=0;
 	//for blocco
-	ret=EVP_DecryptUpdate(ctx, plaintext+plaintext_len, &update_len, ciphertext, ciphertext_len);
+	ret=EVP_DecryptUpdate(ctx, plaintext, &update_len, ciphertext, ciphertext_len);
 	if(ret!=1){cerr<<"DecryptUpdate Error";exit(1);}
 	plaintext_len += update_len;
 
@@ -79,7 +83,6 @@ cbc_decrypt(unsigned char *ciphertext, int ciphertext_len, unsigned char *key, u
 #pragma optimize("",off)
 	memset(ciphertext,0, ciphertext_len);
 #pragma optimize("",on)
-	free(ciphertext);
 
 	return plaintext_len;
 }
