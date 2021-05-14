3d2111d9b272d15ce5395d7bbabc4da3f90dcbcf
Giulio <giulio.silve92@gmail.com>
2021-05-14 17:11:29 +0200

    asymenc+partial payload def 1.2_3

--- security_functions.cpp
+++ security_functions.cpp
@@ -10,6 +10,12 @@
 #include <string.h> 
 using namespace std;
 
+//DigitalEnvelope parameters
+const EVP_CIPHER* DE_cipher = EVP_aes_128_cbc();
+int DE_iv_len = EVP_CIPHER_iv_length(DE_cipher);
+int DE_block_size = EVP_CIPHER_block_size(DE_cipher);
+
+
 void handleErrors(void)
 {
   ERR_print_errors_fp(stderr);
@@ -23,10 +29,108 @@ digsign_verify(){}
 */
 
 //Asymmetric encription and decription. for initial exange and negotiation(?)
-/*
-envelope_seal(){}
-envelope_open(){}
-*/
+unsigned int envelope_seal(EVP_PKEY* peer_pubkey, unsigned char* cleartext, unsigned int clear_size, unsigned char* outputbuffer){
+	int ret;
+	unsigned char *iv = (unsigned char *)malloc(DE_iv_len);
+	if(!iv) {cerr<<"envelope_seal: iv Malloc Error";exit(1);}
+	RAND_poll();
+	ret = RAND_bytes((unsigned char*)&iv[0],DE_iv_len);
+	if(ret!=1){cerr<<"RAND_bytes Error";exit(1);}
+	if(clear_size>UINT_MAX-DE_block_size) {cerr<<"envelope_seal:Integer Overflow Error";exit(1);}
+
+	EVP_CIPHER_CTX *ctx;
+	ctx = EVP_CIPHER_CTX_new();
+	if(!ctx) {cerr<<"envelope_seal: EVP_CIPHER_CTX_new Error";exit(1);}
+	unsigned char* encrypted_key;
+	int encrypted_key_len;
+	encrypted_key= (unsigned char*)malloc(EVP_PKEY_size(peer_pubkey));
+	if(!encrypted_key) {cerr<<"envelope_open: encrypted_key Malloc Error";exit(1);}
+	
+	//Sealinit generates an encrypted_key(symmetric) and random iv
+	ret = EVP_SealInit(ctx, DE_cipher, &encrypted_key, &encrypted_key_len, iv, &peer_pubkey, 1);
+	if(ret!=1){cerr<<"envelope_seal: SealInit Error";exit(1);}
+	unsigned int total_len=encrypted_key_len+DE_iv_len;
+	unsigned int enc_buffer_size = total_len+clear_size+DE_block_size;
+	outputbuffer = (unsigned char*)malloc(enc_buffer_size);
+	if(!outputbuffer) {cerr<<"envelope_seal: outputbuffer Malloc Error";exit(1);}
+	memcpy(outputbuffer,iv,DE_iv_len);
+	memcpy(outputbuffer+DE_iv_len,encrypted_key,encrypted_key_len);
+	int update_len=0;
+
+	ret=EVP_SealUpdate(ctx, outputbuffer+total_len, &update_len, cleartext, clear_size);
+	if(ret!=1){cerr<<"envelope_seal: SealUpdate Error";exit(1);}
+	total_len += update_len;
+	//Encrypt Final. Finalize the encryption and adds the padding
+	ret=EVP_SealFinal(ctx, outputbuffer + total_len, &update_len);
+	if(ret!=1){cerr<<"envelope_seal: SealFinal Error";exit(1);}
+	total_len += update_len;
+	
+	// MUST ALWAYS BE CALLED!!!!!!!!!!
+	EVP_CIPHER_CTX_free(ctx);
+// INIZIO PULIZIA PARAMETRI PASSATI
+#pragma optimize("",off)
+	memset(cleartext,0, clear_size);
+#pragma optimize("",on)
+	free(cleartext);
+// FINE PULIZIA PARAMETRI PASSATI
+	free(encrypted_key);
+	free(iv);
+	return total_len;
+}
+
+unsigned int envelope_open(EVP_PKEY* prvkey, unsigned char* inputbuffer, unsigned int input_size, unsigned char* cleartext){
+	int ret;
+	
+ 	int encrypted_key_len = EVP_PKEY_size(prvkey);
+	if(encrypted_key_len > INT_MAX - DE_iv_len) { cerr << "envelope_open: integer overflow (encrypted key too big?)"; exit(1); }
+	if(input_size < encrypted_key_len + DE_iv_len) { cerr << "Error: encrypted buffer with wrong format\n"; exit(1); }
+	unsigned int cipher_size= input_size - DE_iv_len - encrypted_key_len;
+	if(cipher_size>UINT_MAX-DE_block_size) {cerr<<"envelope_open: cipher_size Integer Overflow Error";exit(1);}
+
+	unsigned char *iv = (unsigned char *)malloc(DE_iv_len);
+	if(!iv) {cerr<<"envelope_open: iv Malloc Error";exit(1);}
+	unsigned char* encrypted_key = (unsigned char*)malloc(encrypted_key_len);
+	if(!encrypted_key) {cerr<<"envelope_open: encrypted_key Malloc Error";exit(1);}
+	unsigned char* ciphertext =(unsigned char*)malloc(cipher_size);
+	if(!ciphertext) {cerr<<"envelope_open: ciphertext Malloc Error";exit(1);}
+	memcpy(iv,inputbuffer,DE_iv_len);
+	memcpy(encrypted_key,inputbuffer+DE_iv_len,encrypted_key_len);
+	memcpy(ciphertext,inputbuffer+DE_iv_len+encrypted_key_len,cipher_size);
+	cleartext = (unsigned char*)malloc(cipher_size);
+	if(!cleartext) {cerr<<"envelope_open: cleartext Malloc Error";exit(1);}
+
+	EVP_CIPHER_CTX *ctx;
+	ctx = EVP_CIPHER_CTX_new();
+	if(!ctx) {cerr<<"envelope_open: EVP_CIPHER_CTX_new Error";exit(1);}
+	// Encrypt init
+	ret=EVP_OpenInit(ctx, DE_cipher, encrypted_key, encrypted_key_len, iv, prvkey);
+	if(ret!=1){cerr<<"envelope_open: OpenInit Error";exit(1);}
+	unsigned int total_len=0;
+	int update_len=0;
+	// Encrypt Update: one call is enough because our message is very short.
+	ret=EVP_OpenUpdate(ctx, cleartext, &update_len, ciphertext, cipher_size);
+	if(ret!=1){cerr<<"envelope_open: OpenUpdate Error";exit(1);}
+	total_len += update_len;
+	//Encrypt Final. Finalize the encryption and adds the padding
+	ret=EVP_OpenFinal(ctx, cleartext + total_len, &update_len);
+	if(ret!=1){cerr<<"envelope_open: OpenFinal Error";exit(1);}
+	total_len += update_len;
+	
+
+	EVP_CIPHER_CTX_free(ctx);
+// INIZIO PULIZIA PARAMETRI PASSATI
+#pragma optimize("",off)
+	memset(inputbuffer,0, input_size);
+#pragma optimize("",on)
+	free(inputbuffer);
+// FINE PULIZIA PARAMETRI PASSATI
+	free(ciphertext);
+	free(encrypted_key);
+	free(iv);
+   	return total_len;
+}
+
+
 // Diffie-Hellman for session key
 EVP_PKEY* dh_generate_key(char* buffer,unsigned int &buffersize){
 
@@ -53,8 +157,9 @@ EVP_PKEY* dh_generate_key(char* buffer,unsigned int &buffersize){
 	if(1 != EVP_PKEY_keygen_init(DHctx)) handleErrors();
 	if(1 != EVP_PKEY_keygen(DHctx, &my_dhkey)) handleErrors();
 	
-/* Write into a file*/
-	if(!BIO* bio = BIO_new(BIO_s_mem())) { cerr<<"dh_generate_key: Failed to allocate BIO_s_mem";exit(1); }
+/* Write into a buffer*/
+	BIO* bio = BIO_new(BIO_s_mem());
+	if(!bio) { cerr<<"dh_generate_key: Failed to allocate BIO_s_mem";exit(1); }
 	if(!PEM_write_bio_PUBKEY(bio,  my_dhkey)) { cerr<<"dh_generate_key: PEM_write_bio_PUBKEY error";exit(1); }
 	char* buf=NULL;
 	long size = BIO_get_mem_data(bio, &buffer);
@@ -62,6 +167,7 @@ EVP_PKEY* dh_generate_key(char* buffer,unsigned int &buffersize){
 	memcpy(buffer+buffersize,buf,size);
 	buffersize+=size;
 	BIO_free(bio);
+
 	return my_dhkey;
 
 } 
@@ -81,8 +187,10 @@ unsigned int dh_derive_shared_secret(EVP_PKEY* peer_pub_key, EVP_PKEY* my_prv_ke
 	if (!shared_secret) handleErrors();
 	/*Perform again the derivation and store it in shared_secret buffer*/
 	if (EVP_PKEY_derive(derive_ctx, shared_secret, &shared_secretlen) <= 0) handleErrors();
+// INIZIO PULIZIA PARAMETRI PASSATI
 	EVP_PKEY_free(peer_pub_key);
 	EVP_PKEY_free(my_prv_key);
+// FINE PULIZIA PARAMETRI PASSATI
 	EVP_PKEY_CTX_free(derive_ctx);
 	
 	return (unsigned int)shared_secretlen;
@@ -91,7 +199,7 @@ unsigned int dh_derive_shared_secret(EVP_PKEY* peer_pub_key, EVP_PKEY* my_prv_ke
 
 unsigned int dh_generate_session_key(unsigned char *shared_secret,unsigned int shared_secretlen, unsigned char *sessionkey){
 	unsigned int sessionkey_len;
-	int ret
+	int ret;
 	EVP_MD_CTX* hctx;
 	
 	/* Buffer allocation for the digest */
@@ -103,7 +211,7 @@ unsigned int dh_generate_session_key(unsigned char *shared_secret,unsigned int s
 	/* Hashing (initialization + single update + finalization */
 	ret=EVP_DigestInit(hctx, EVP_sha256());
 	if(ret!=1){cerr<<"dh_generate_session_key: EVP_DigestInit Error";;exit(1);}
-	ret=EVP_DigestUpdate(hctx, shared_secretkey, shared_secretlen);
+	ret=EVP_DigestUpdate(hctx, shared_secret, shared_secretlen);
 	if(ret!=1){cerr<<"dh_generate_session_key: EVP_DigestUpdate Error";;exit(1);}
 	ret=EVP_DigestFinal(hctx, sessionkey, &sessionkey_len);
 	if(ret!=1){cerr<<"dh_generate_session_key: EVP_DigestFinal Error";;exit(1);}
